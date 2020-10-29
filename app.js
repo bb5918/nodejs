8f9a81a64c5e41f223b3db9c3a1818c2fedba42a
AzureAD\LEEEUNBI <eunbi5918@naver.com>
2020-10-29 22:42:35 +0900

    10/29

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const nunjucks = require("nunjucks");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // db 관련
-const db = require('./models');
+const db = require("./models");
 
 // DB authentication
-db.sequelize.authenticate()
-.then(() => {
-    console.log('Connection has been established successfully.');
-    return db.sequelize.sync()
-})
-.then(() => {
-    console.log('DB Sync complete.');
-})
-.catch(err => {
-    console.error('Unable to connect to the database:', err);
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+    return db.sequelize.sync();
+  })
+  .then(() => {
+    console.log("DB Sync complete.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+const admin = require("./routes/admin");
+
+const app = express();
+const port = 3000;
+
+nunjucks.configure("template", {
+  autoescape: true,
+  express: app,
 });
 
-const admin = require('./routes/admin')
-
-const app = express()
-const port = 3000
-
-nunjucks.configure('template',{
-    autoescape:true,
-    express:app
-})
-
 // 미들웨어 셋팅
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get('/',function(req,res){
-    res.send('first app2233332')
-})
+//업로드 path 추가
+app.use("/uploads", express.static("uploads"));
 
-app.use('/admin',admin)
+app.get("/", function (req, res) {
+  res.send("first app2233332");
+});
 
-app.listen(port,function(){
-    console.log('Express listening on port',port)
-})
+app.use("/admin", admin);
+
+app.listen(port, function () {
+  console.log("Express listening on port", port);
+});
